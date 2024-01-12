61ea01499f369fa3f1d6e72513ae9cced335d2b1
Henok-Al <henok.gebresenbet@gmail.com>
2024-01-13 01:18:15 +0300

    implementing login api

---
+++ server/seed.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import bcrypt from 'bcrypt'
+import { Admin } from './models/Admin.js'
+import './db.js'
+
+async function AdminAccount(){
+try {
+    const adminCount =await Admin.countDocuments()
+    //if there is no record we will create new record for admin
+    if(adminCount === 0) {
+        const hashPassword = await bcrypt.hash('adminpassword', 10)
+        const newAdmin =new Admin({
+            username: "admin",
+            password : hashPassword,
+        })
+        newAdmin.save()
+        console.log("account created")
+    } else {
+        console.log("account already existed")
+    }
+} catch (error) {
+    console.log("error")
+}
+}
+
+AdminAccount()
