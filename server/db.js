3d54426818d6db19c0178dbb7ce7ae7be91cd166
Henok-Al <henok.gebresenbet@gmail.com>
2024-07-05 16:23:04 +0300

    Removed node_modules folder

--- server/db.js
+++ server/db.js
@@ -2,11 +2,11 @@ import { mongoose } from "mongoose";
 import dotenv from 'dotenv'
 dotenv.config()
 
-//connect to database
+//connect to mongodb database
 const Connection = async() => {
     try {
         mongoose.connect(process.env.URL)
-        
+
         console.log("connected to mongodb")
     } catch (err) {
         console.log("Error: " + err)
